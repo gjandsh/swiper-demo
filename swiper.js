69fe128730f7bc4284e8637f4d166d5d3f35e25f
gjandsh <gjandsh@126.com>
2017-04-29 19:40:21 +0800

    增加环形轮播，增加li激活样式

--- swiper.js
+++ swiper.js
@@ -5,7 +5,6 @@ window.slides = function(element){
   var count = $el.find('.slide').length
   var currentIndex = 0
   let timerId
-  
 
   function controls(){
     var $ol = $('<ol class="controls"></ol>')
@@ -13,12 +12,12 @@ window.slides = function(element){
     $ol.append(`<li>${i+1}</li>`)
     }
     $el.append($ol)
- 
+    $item = $ol.find('li')
+    $item.eq(0).addClass('active');
     $ol.on('click','li', function(e){ 
     let $li = $(e.currentTarget)
     let index = $li.index()
       goToSlide(index)
-
     })
   }
    
@@ -30,30 +29,47 @@ window.slides = function(element){
     }
     let number = - width * index
     $view.css({transform: `translateX(${number}px)`})
+    cyclic(index)
+    $item.removeClass('active');
+    $item.eq(index).addClass('active');
+    currentIndex = index
     currentIndex=index
   }
 
+  function cyclic(index){
+    if(index === 0&&currentIndex===count-1){
+      let $li = $el.find('.slide').eq(0).clone()
+      $li.appendTo($view)
+      let number = - width * count
+      $view.css({transform: `translateX(${number}px)`})
+      $view.one("transitionend", function(){
+      $li.remove()
+      let oldTransition = $view.css('transition')
+      $view.css({
+      transition: 'none',
+      transform: `translateX(0px)`
+      })
+      $view.offset()
+      $view.css('transition', oldTransition)
+      })
+     return
+    }
+  }
+  
   function autoPlay(){
     timerId = setInterval(function(){
-    goToSlide(currentIndex)
-   // console.log(currentIndex)
-    currentIndex++
-
-    if(currentIndex>=count){
-      currentIndex=0
-    }
+    goToSlide(currentIndex+1)
     },2000)
   }
-   $el.on('mouseenter', function(){
+   $view.on('mouseenter', function(){
      window.clearInterval(timerId)
    })    
-   $el.on('mouseleave', function(){
+   $view.on('mouseleave', function(){
      autoPlay()
    })
 
  controls()
- autoPlay()
-
+ //autoPlay()
   
 }
 
